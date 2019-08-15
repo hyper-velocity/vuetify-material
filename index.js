09a754c4642655b723d94946f432e437890c8391
webassassin <grublack7@gmail.com>
2019-08-15 06:58:59 -0700

    removed theme from vuetify-material

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'chartist/dist/chartist.min.css'
 
-export const theme = {
-  light: {
-    primary: '#1976d2',
-    secondary: '#4caf50',
-    tertiary: '#495057',
-    accent: '#82B1FF',
-    error: '#f55a4e',
-    info: '#00d3ee',
-    success: '#5cb860',
-    warning: '#ffa21a'
-  }
-}
-
 Vue.use(Vuetify)
 Vue.use(require('vue-chartist'))
 
@@ -28,12 +15,10 @@ export const config = () => {
   return new Vuetify({
     icons: {
       iconfont: 'mdi'
-    },
-    theme
+    }
   })
 }
 
 export default {
-  theme,
   config
 }
