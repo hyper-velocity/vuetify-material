b28f15be5c2df8cadef212fce86e029a33eaeeef
webassassin <grublack7@gmail.com>
2019-08-14 20:26:15 -0700

    updates on module entry

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import './styles/index.scss'
 import './components'
 
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'chartist/dist/chartist.min.css'
+
+export const theme = {
+  light: {
+    primary: '#1976d2',
+    secondary: '#4caf50',
+    tertiary: '#495057',
+    accent: '#82B1FF',
+    error: '#f55a4e',
+    info: '#00d3ee',
+    success: '#5cb860',
+    warning: '#ffa21a'
+  }
+}
+
+Vue.use(Vuetify)
+Vue.use(require('vue-chartist'))
+
+const config = () => {
+  return new Vuetify({
+    icons: {
+      iconfont: 'mdi'
+    },
+    theme
+  })
+}
+
+export default {
+  theme,
+  config
+}
