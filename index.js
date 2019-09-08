db95666470280d920406cc183e0c4cdd5dd3760b
webassassin <grublack7@gmail.com>
2019-09-08 08:44:39 -0700

    style backup

--- index.js
+++ index.js
@@ -14,5 +14,17 @@ Vue.use(require('vue-chartist'))
 export default new Vuetify({
   icons: {
     iconfont: 'mdi'
+  },
+  theme: {
+    light: {
+      primary: '#4caf50',
+      secondary: '#4caf50',
+      tertiary: '#495057',
+      accent: '#82B1FF',
+      error: '#f55a4e',
+      info: '#00d3ee',
+      success: '#5cb860',
+      warning: '#ffa21a'
+    }
   }
 })
